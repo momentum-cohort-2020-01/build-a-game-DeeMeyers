99d01fba0326b5351f3c5395763a929b91d75312
DeeMeyers <justdmeyers@gmail.com>
2020-02-12 13:39:51 -0500

    hooww?

--- games.js
+++ games.js
@@ -3,3 +3,9 @@ const context =  canvas.getContext('2d')
 
 context.font = '40pt helvetica'
 context.fillText("howdy!", 100, 100)
+
+context.beginPath()
+context.moveTo(15,15)
+context.lineTo(35, 45)
+context.lineWidth = 3
+context.stroke()
