e628aa84b35cde75db2912c958a26dcb634531a9
DeeMeyers <justdmeyers@gmail.com>
2020-02-13 10:17:59 -0500

    hmmm

--- games.js
+++ games.js
@@ -1,11 +1,49 @@
-const canvas = document.querySelector('#canvas')
-const context =  canvas.getContext('2d')
 
-context.font = '40pt helvetica'
-context.fillText("howdy!", 100, 100)
 
-context.beginPath()
-context.moveTo(15,15)
-context.lineTo(35, 45)
-context.lineWidth = 3
-context.stroke()
+class Game {
+    constructor() {
+        const canvas = document.querySelector('#canvas')
+        const screen =  canvas.getContext('2d')
+        canvas.fillStyle = "yellow"
+        let gameSize = { x: canvas.width, y: canvas.height }
+        this.bodies = []
+        this.bodies = this.bodies.concat(new Player(this, gameSize))
+
+
+        let tick = () => {
+            
+            this.draw(screen, gameSize)
+            requestAnimationFrame(tick)
+        }
+        tick()
+    }
+
+    draw (screen, gameSize) {
+        // Clear away the drawing from the previous tick.
+        screen.clearRect(0, 0, gameSize.x, gameSize.y)
+        fillStyle = "blue"
+        // Draw each body as a rectangle.
+        for (let i = 0; i < this.bodies.length; i++) {
+        drawRect(screen, this.bodies[i])
+        }
+    
+    
+    
+}
+}
+class Player {
+    constructor (game, gameSize) {
+        this.game = game
+        this.size = { x: 15, y: 15 }
+        this.center = { x: gameSize.x/2, y: gameSize.y/2 }
+
+    
+    }
+}
+
+function drawRect (screen, body) {
+    screen.fillRect(10, 10, 25, 25)
+
+}
+
+new Game()
